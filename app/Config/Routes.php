980d1a299ee990924091da51f7097c6024ee4dbe
unknown <andayajoshua96@gmail.com>
2023-01-28 22:12:57 +0800

    nav

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,7 +29,6 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
 $routes->get('/dashboard', 'Dashboard::index');
 $routes->get('/login', 'Login::index');
 $routes->get('/visitor-information', 'VisitorInformation::index');
